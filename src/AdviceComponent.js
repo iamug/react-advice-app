825c9da5c01dbdbd029bffafefd18fc6dc6da349
Aguziendu Ugochukwu <uaguziendu@gmail.com>
2021-04-13 07:48:44 +0100

    bug fixes

---
+++ src/AdviceComponent.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const AdviceComponent = () => {
+  const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchadvice = async () => {
+    setLoading(false);
+    let response = await axios.get("https://api.adviceslip.com/advice");
+    console.log({ response });
+    const { advice } = response.data.slip;
+    setAdvice(advice);
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    fetchadvice();
+    //fetch();
+    //   return () => {
+    //       cleanup
+    //   }
+  }, []);
+
+  return (
+    <div className="container">
+      {loading ? (
+        <>
+          <h2> {advice} </h2>
+          <button onClick={() => fetchadvice()}> Give me advice </button>
+        </>
+      ) : (
+        <img src="loading.gif" alt="loading" />
+      )}
+    </div>
+  );
+};
+
+export default AdviceComponent;
